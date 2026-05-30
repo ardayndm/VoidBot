294ce231cd12b50758b3685e371762230651639e
Arda <138328104+ardayndm@users.noreply.github.com>
2026-05-31 01:49:42 +0300

    Yeni ikonlar

--- events/core/registry.go
+++ events/core/registry.go
@@ -68,7 +68,11 @@ func HandleMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	prefix := config.AppConfig.Bot.Prefix
-	
+
+	utils.RespondSuccessEmbed(s, utils.Target{
+		Message: m,
+	}, "default", config.GetBot().Name)
+
 	// Prefix ile başlıyor mu?
 	if !strings.HasPrefix(m.Content, prefix) {
 		return
